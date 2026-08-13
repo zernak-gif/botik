da874c65c95a2f3fdcf72ea2e9d17c055d721abb
zernak-gif <zergornak@gmail.com>
2026-08-13 20:09:33 +0300

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # === НАСТРОЙКИ ===
-BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+BOT_TOKEN = os.environ.get("8793997691:AAGNe0PQs674SYYnNLwdr9giqAeb-8wfC0o")
 ADMIN_ID = 976653458
 
 # Проверка загрузки переменных
